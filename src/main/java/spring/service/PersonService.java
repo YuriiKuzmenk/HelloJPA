4b0baf21b354bae25441617f6b3524ac50826139
Yurii Kuzmenko <108761481+YuriiKuzmenk@users.noreply.github.com>
2023-03-25 19:33:30 +0100

    Java Project Hello JPA

--- src/main/java/spring/service/PersonService.java
+++ src/main/java/spring/service/PersonService.java
@@ -1,2 +1,23 @@
-package spring.service;public class PersonService {
+package spring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import spring.domain.Person;
+import spring.repository.PersonRepository;
+
+import java.util.List;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    public Iterable<Person> findAll(){
+        return personRepository.findAll();
+    }
+
+    public void add (Person person) {
+        personRepository.save(person);
+    }
 }
